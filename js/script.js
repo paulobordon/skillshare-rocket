a744c451a835fcd48497edfd760908a61e3347ae
Paulo Bordón <bordonbp@gmail.com>
2017-03-17 17:42:56 -0300

    actualizaciones
    se agrega css y javascript

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//js archivo
+
+		console.log('Esto debe aparecer en la consola');
+
+//cambiar color de una caja 
+
+	var colorChange = function() {
+		var nombreActual = document.getElementById('box1').className;
+
+		if (nombreActual
+		//document.getElementById('box1').className = 'caja rojo';
+	}
+
+//primer codigo de prueba 
+
+		var auto = {
+			marca: 'Suzuki',
+			tipo: 'Szwift',
+			color: 'gris',
+			estaPrendido: false,
+			numDeRuedas: 4,
+			asientos: [
+				'asiento1',
+				'asiento2',
+				'asiento3',
+				'asiento4',
+				'asiento5'
+			],
+			prender : function() {
+				this.estaPrendido = true;
+			},
+			apagar : function() {
+				this.estaPrendido = false;
+			},
+			// cambiarColor : function (color) {
+			// 	if (color == 'gris'){
+			// 		alert ('El auto es de color ' + color);
+			// 	} else {
+			// 		alert ('Te equivocaste de Auto');
+			// 	}
+			// }
+			cambiarColor : function (color) {
+				if (color == 'gris') {
+					alert ('El auto ya es ' + color);
+				} else if (color != 'gris') {
+					alert ('Haz cambiado tu auto al color ' + color);
+				}
+			}
+
+		};
