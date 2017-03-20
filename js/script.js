20839e3b7db7dc2049ea141ebc85196761b55bf4
Paulo Bordón <bordonbp@gmail.com>
2017-03-20 17:17:08 -0300

    Cohete funcional
    html, css y js del cohete. Falta agregar ilustración y otros ajustes al
    cohete.

--- js/script.js
+++ js/script.js
@@ -1,80 +1,44 @@
 //js archivo
 
-		console.log('Esto debe aparecer en la consola');
+		console.log('HOLA COHETE');
 
-//cambiar color de una caja 
+//funciones variables
+var timer = null;
+var cuentaRegresiva = 10;
+//arriba determino las variables del temporizador y de la cuenta regresiva, al que le doy un valor
 
-	var showHide = function() {
-		var nombreActual = document.getElementById('box1').className;
+var cambiaEstado = function(estado) {
+	document.body.className = 'body-stage'+estado;
+	clearInterval(timer);
+	cuentaRegresiva = 10;
+	document.getElementById('cuentabajo').innerHTML = cuentaRegresiva;
+//en esta función, lo que digo es que los que tenga evento con la variable/funcion "cambiaEstado", donde dentro del parentesis se va a encontrar los valores de cada estado.
+//Para cambiar la clase del BODY, que son 'estados', digo que agarre el Body con la clase 'body-stage' más el estado.
+//Al hacer click en el evento, como los parametros están definidos en el HTML, cambia el body de estado
+	if (estado == 2) {
+		timer = setInterval(function(){
+			cuentaRegresiva = cuentaRegresiva -1;
+			document.getElementById('cuentabajo').innerHTML = cuentaRegresiva;
+			if (cuentaRegresiva <= -1){
+				cambiaEstado(3);
+			}
 
-		if (nombreActual == 'caja') {
-			document.getElementById('box1').className = 'caja show';
-		} else {
-			document.getElementById('box1').className = 'caja'
-		}  
+		}, 1000);
+//En esta funcion digo que si el ESTADO del body es igual a 2, que inicie el timer. Donde con la funcion setInterval() determino otra función donde digo que agarre el elemento con id 'cuentabajo' y con el innerHTML, digo que agarre la variable cuentaRegresiva. Luego, digo que el valor de cuentaRegresiva sea igual al valor del mismo menos 1. Al final, digo que esta funcion de cuentaRegresiva sea realizada cada 1000 ms, o sea, cada segundo
+	} else if (estado == 3){
 
-
-		//document.getElementById('box1').className = 'caja rojo';
+		var exito = setTimeout (function() {
+			var randomNumber = Math.round(Math.random()*10);
+			console.log('randomNumber: ',randomNumber);
+			if (randomNumber > 3) {
+				//exito
+				cambiaEstado(4)
+			} else {
+				//FAIL
+				cambiaEstado(5)
+			}
+		}, 3000);
 	};
 
-	var cambiarPalabra = function() {
-		var claseActual = document.getElementById('box1').className;
+}
 
-		if (claseActual == 'caja show') {
-			document.getElementById('cambiarPalabra').innerHTML = "OCULTAR";	
-		} else {
-			document.getElementById('cambiarPalabra').innerHTML = "MOSTRAR";
-		}
-		
-	}
-
-	var colorChange = function() {
-		var nombreActual = document.getElementById('box1').className;
-
-		if (nombreActual == 'caja show') {
-			document.getElementById('box1').className = 'caja show color';
-		} else {
-			document.getElementById('box1').className = 'caja show'
-		}  
-
-
-		//document.getElementById('box1').className = 'caja rojo';
-	};
-
-//primer codigo de prueba 
-
-		// var auto = {
-		// 	marca: 'Suzuki',
-		// 	tipo: 'Szwift',
-		// 	color: 'gris',
-		// 	estaPrendido: false,
-		// 	numDeRuedas: 4,
-		// 	asientos: [
-		// 		'asiento1',
-		// 		'asiento2',
-		// 		'asiento3',
-		// 		'asiento4',
-		// 		'asiento5'
-		// 	],
-		// 	prender : function() {
-		// 		this.estaPrendido = true;
-		// 	},
-		// 	apagar : function() {
-		// 		this.estaPrendido = false;
-		// 	},
-		// 	// cambiarColor : function (color) {
-		// 	// 	if (color == 'gris'){
-		// 	// 		alert ('El auto es de color ' + color);
-		// 	// 	} else {
-		// 	// 		alert ('Te equivocaste de Auto');
-		// 	// 	}
-		// 	// }
-		// 	cambiarColor : function (color) {
-		// 		if (color == 'gris') {
-		// 			alert ('El auto ya es ' + color);
-		// 		} else if (color != 'gris') {
-		// 			alert ('Haz cambiado tu auto al color ' + color);
-		// 		}
-		// 	}
-
-		// };
---
+++ js/test-script.js
@@ -0,0 +1,80 @@
+//js archivo
+
+		console.log('Esto debe aparecer en la consola');
+
+//cambiar color de una caja 
+
+	var showHide = function() {
+		var nombreActual = document.getElementById('box1').className;
+
+		if (nombreActual == 'caja') {
+			document.getElementById('box1').className = 'caja show';
+		} else {
+			document.getElementById('box1').className = 'caja'
+		}  
+
+
+		//document.getElementById('box1').className = 'caja rojo';
+	};
+
+	var cambiarPalabra = function() {
+		var claseActual = document.getElementById('box1').className;
+
+		if (claseActual == 'caja show') {
+			document.getElementById('cambiarPalabra').innerHTML = "OCULTAR";	
+		} else {
+			document.getElementById('cambiarPalabra').innerHTML = "MOSTRAR";
+		}
+		
+	}
+
+	var colorChange = function() {
+		var nombreActual = document.getElementById('box1').className;
+
+		if (nombreActual == 'caja show') {
+			document.getElementById('box1').className = 'caja show color';
+		} else {
+			document.getElementById('box1').className = 'caja show'
+		}  
+
+
+		//document.getElementById('box1').className = 'caja rojo';
+	};
+
+//primer codigo de prueba 
+
+		// var auto = {
+		// 	marca: 'Suzuki',
+		// 	tipo: 'Szwift',
+		// 	color: 'gris',
+		// 	estaPrendido: false,
+		// 	numDeRuedas: 4,
+		// 	asientos: [
+		// 		'asiento1',
+		// 		'asiento2',
+		// 		'asiento3',
+		// 		'asiento4',
+		// 		'asiento5'
+		// 	],
+		// 	prender : function() {
+		// 		this.estaPrendido = true;
+		// 	},
+		// 	apagar : function() {
+		// 		this.estaPrendido = false;
+		// 	},
+		// 	// cambiarColor : function (color) {
+		// 	// 	if (color == 'gris'){
+		// 	// 		alert ('El auto es de color ' + color);
+		// 	// 	} else {
+		// 	// 		alert ('Te equivocaste de Auto');
+		// 	// 	}
+		// 	// }
+		// 	cambiarColor : function (color) {
+		// 		if (color == 'gris') {
+		// 			alert ('El auto ya es ' + color);
+		// 		} else if (color != 'gris') {
+		// 			alert ('Haz cambiado tu auto al color ' + color);
+		// 		}
+		// 	}
+
+		// };
