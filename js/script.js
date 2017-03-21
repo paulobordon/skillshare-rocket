c1c2d555de7c08cf19fce4d026d943df1f01060a
Paulo Bordón <bordonbp@gmail.com>
2017-03-21 17:34:32 -0300

    the rocket is finished!
    se añaden los últimos estilos para la sección de lanzamiento. Botones y
    efectos css

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ var cambiaEstado = function(estado) {
 		var exito = setTimeout (function() {
 			var randomNumber = Math.round(Math.random()*10);
 			console.log('randomNumber: ',randomNumber);
-			if (randomNumber > 3) {
+			if (randomNumber > 4) {
 				//exito
 				cambiaEstado(4)
 			} else {
